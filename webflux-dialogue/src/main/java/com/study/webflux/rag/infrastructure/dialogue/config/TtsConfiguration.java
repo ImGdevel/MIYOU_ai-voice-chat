2654ba925e0da1019580b3c23da79adf43fd1d59
SH Woo <imdlsrks.mc@gmail.com>
2025-12-30 21:56:33 +0900

    🐛 fix: SupertoneConfig 빈 복원

--- webflux-dialogue/src/main/java/com/study/webflux/rag/infrastructure/dialogue/config/TtsConfiguration.java
+++ webflux-dialogue/src/main/java/com/study/webflux/rag/infrastructure/dialogue/config/TtsConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import com.study.webflux.rag.domain.dialogue.port.TtsPort;
 import com.study.webflux.rag.domain.voice.model.Voice;
 import com.study.webflux.rag.infrastructure.dialogue.adapter.tts.LoadBalancedSupertoneTtsAdapter;
+import com.study.webflux.rag.infrastructure.dialogue.adapter.tts.SupertoneConfig;
 import com.study.webflux.rag.infrastructure.dialogue.adapter.tts.loadbalancer.TtsEndpoint;
 import com.study.webflux.rag.infrastructure.dialogue.adapter.tts.loadbalancer.TtsLoadBalancer;
 import com.study.webflux.rag.infrastructure.dialogue.config.properties.RagDialogueProperties;
@@ -21,6 +22,16 @@ import org.slf4j.LoggerFactory;
 public class TtsConfiguration {
 
 	private static final Logger log = LoggerFactory.getLogger(TtsConfiguration.class);
+
+	@Bean
+	public SupertoneConfig supertoneConfig(RagDialogueProperties properties) {
+		var supertone = properties.getSupertone();
+		if (supertone.getEndpoints().isEmpty()) {
+			throw new IllegalStateException("최소 하나 이상의 TTS 엔드포인트를 설정해야 합니다");
+		}
+		var firstEndpoint = supertone.getEndpoints().get(0);
+		return new SupertoneConfig(firstEndpoint.getApiKey(), firstEndpoint.getBaseUrl());
+	}
 
 	@Bean
 	public TtsLoadBalancer ttsLoadBalancer(RagDialogueProperties properties) {
