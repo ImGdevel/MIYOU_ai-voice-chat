152e8f0ce02e0f5c9ae876142247df4fcd0fbbd0
SH Woo <imdlsrks.mc@gmail.com>
2026-03-13 11:30:00 +0900

    feat(domain): 미션 도메인 모델 및 포트 추가 Mission, UserMission, MissionStatus, MissionType 등 미션 시스템의 핵심 도메인 모델과 리포지토리 포트를 정의한다.

---
+++ webflux-dialogue/src/main/java/com/study/webflux/rag/domain/mission/model/MissionStatus.java
@@ -0,0 +1,7 @@
+package com.study.webflux.rag.domain.mission.model;
+
+public enum MissionStatus {
+	AVAILABLE,
+	COMPLETED,
+	REWARDED
+}
---
+++ webflux-dialogue/src/main/java/com/study/webflux/rag/domain/mission/model/MissionType.java
@@ -0,0 +1,9 @@
+package com.study.webflux.rag.domain.mission.model;
+
+public enum MissionType {
+	SIGNUP_BONUS,
+	SHARE_SERVICE,
+	TASK_COMPLETION,
+	REFERRAL,
+	CUSTOM
+}
